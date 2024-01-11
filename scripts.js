8cca7ffd3970a02177ff743735937039b50af4f1
Maiane <maiababauer@gmail.com>
2024-01-11 16:31:33 -0300

    atualização

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let header = document.getElementById('header')
 window.addEventListener('scroll', () => {
 if(window.scrollY >= 200){
-header.style.background = '#4A766E'
+header.style.background = '#ff1493'
 } else {
 header.style.background = 'transparent'
 }
